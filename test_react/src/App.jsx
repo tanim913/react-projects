8483cfff33ed98a6c003a045cb5d12d4a63b2e72
tanim913 <tanim.khaled24@gmail.com>
2024-11-06 03:14:43 +0600

    [ADD] react bootrap and routing

--- test_react/src/App.jsx
+++ test_react/src/App.jsx
@@ -1,13 +1,37 @@
+import './App.css';
+import './home.scss'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import BootstrapCard from './components/BootstrapCard.jsx';
+import Card from './components/Card.jsx';
+import ClassCard from './components/ClassCard.jsx';
+import NestedCard from './components/NestedCard.jsx';
+import CountryData from './data.json';
+import UserData from './user_data.json';
+import { v4 as uuidv4 } from "uuid";
 
-import './App.css'
-import Card from './components/Card.jsx'
-import ClassCard from './components/ClassCard.jsx'
-import NestedCard from './components/NestedCard.jsx'
-import CountryData from './data.json'
-import UserData from './user_data.json'
-import { v4 as uuidv4} from "uuid";
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the Card Viewer</h1>
+      <div className="buttons-container">
+        <Link to="/card">
+          <button className='btn-home'>Card</button>
+        </Link>
+        <Link to="/nested-card">
+          <button className='btn-home'>Nested Card</button>
+        </Link>
+        <Link to="/class-card">
+          <button className='btn-home'>Class Card</button>
+        </Link>
+        <Link to="/bootstrap-card">
+          <button className='btn-home'>Bootstrap Card</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
-function App() {
+function CardPage() {
   return (
     <>
       {CountryData.map((item) => (
@@ -20,22 +44,37 @@ function App() {
           language={item.Language}
         />
       ))}
-      
-      <div className="nested-card-container">
-        {UserData.map((user, index) => (
-          <NestedCard
-            key={index}
-            fullname={user.fullname}
-            age={user.age}
-            phones={user.phones}
-          />
-        ))}
-      </div>
-
-      {/* Class component */}
-      <ClassCard name="Class component passing props" />
     </>
-  )
+  );
+}
+
+function NestedCardPage() {
+  return (
+    <div className="nested-card-container">
+      {UserData.map((user, index) => (
+        <NestedCard
+          key={index}
+          fullname={user.fullname}
+          age={user.age}
+          phones={user.phones}
+        />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/card" element={<CardPage />} />
+        <Route path="/nested-card" element={<NestedCardPage />} />
+        <Route path="/class-card" element={<ClassCard name="Class component passing props" />} />
+        <Route path="/bootstrap-card" element={<BootstrapCard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
